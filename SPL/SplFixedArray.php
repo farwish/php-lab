c62a65f008dbd98ef4e648f52a6ee9b6c1c37174
farwish <farwish@foxmail.com>
2017-03-13 12:49:44 +0800

    annotate

--- SPL/SplFixedArray.php
+++ SPL/SplFixedArray.php
@@ -1,28 +1,47 @@
 <?php
 
 /**
- * 同数据量:
- *  SplFixedArray 更省内存.
- *  用户调用和系统调用两者基本一样.
+ * 同数据量情况:
+ * 
+ * 数据量很小的时候（循环10次）：
+ *  用户调用和系统调用基本一致.
+ *  内存占用一致.
+ *  Array 稍快.
+ *
+ * 数据量中等的时候（循环100次）：
+ *  用户调用和系统调用基本一致.
+ *  内存占用基本一致，SplFixedArray 稍少.
+ *  Array 稍快.
+ *
+ * 数据量较大的时候（循环>1000次）:
+ *  用户调用和系统调用基本一致.
+ *  SplFixedArray 内存占用少.
+ *  SplFixedArray 更快
  *
  * @farwish
  */
 
-/*
+$start = microtime();
+
 $arr = [];
 
-for ($i = 0; $i < 100; $i++)
+/*
+for ($i = 0; $i < 10000; $i++)
 {
     $arr[] = 10;
 }
  */
 
-$arr = new SplFixedArray(100);
+$arr = new SplFixedArray(10000);
 
-for ($i = 0; $i < 100; $i++)
+for ($i = 0; $i < 10000; $i++)
 {
     $arr[$i] = 10;
 }
 
-echo memory_get_usage();
+$end = microtime();
+
+echo "Memory use: " . memory_get_usage()/1024 . " KB\n";
+
+echo "Time spend: " . ($end - $start) . "\n";
 
