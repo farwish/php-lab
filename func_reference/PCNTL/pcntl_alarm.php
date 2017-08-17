19779c743413223c1e2412044b7ad9a027843c09
farwish <farwish@foxmail.com>
2017-08-17 20:05:30 +0800

    pcntl_alarm(0)

--- func_reference/PCNTL/pcntl_alarm.php
+++ func_reference/PCNTL/pcntl_alarm.php
@@ -3,7 +3,7 @@
  * 为信号设置闹钟(定时)信号.
  *
  * 设置一个定时器(timer)给进程发送 SIGALRM 信号.
- * 调用 pcntl_alarm() 会清除之前设置的闹钟.
+ * 调用 pcntl_alarm() 会清除之前设置的闹钟，如 pcntl_alarm(0)
  *
  * @link http://cn2.php.net/manual/en/function.pcntl-alarm.php
  * @license Apache
@@ -11,7 +11,7 @@
  */
 
 // 1.
-if (! pcntl_signal(SIGALRM, 'my_handler') ) {
+if (! pcntl_signal(SIGALRM, ['\Test', 'handler']) ) {
     echo "Set signal handler failure" . PHP_EOL;
     die;
 }
@@ -21,6 +21,11 @@ if (! pcntl_signal(SIGALRM, 'my_handler') ) {
 // 相当于3秒后执行 posix_kill( posix_getpid(), SIGALRM )
 // 返回值：在此之前用 pcntl_alarm 设置的时间，没有则为0
 pcntl_alarm(3);
+
+// pcntl_alarm(0);
+// Will cancel any previously set alarm.
+// If seconds is zero, no new alarm is created.
+// 本行作用只是清除之前的alarm.
 
 sleep(3);
 
@@ -37,5 +42,21 @@ function my_handler($signo)
 
         default:
             break;
+    }
+}
+
+class Test
+{
+    public static function handler($signo)
+    {
+        switch ($signo) {
+            case SIGALRM:
+
+                echo "Caught SIGALRM ..." . PHP_EOL;
+                break;
+
+            default:
+                break;
+        }
     }
 }
