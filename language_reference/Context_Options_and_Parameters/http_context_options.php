ba04715f876840044ecd59b5c9c5f1e8bef581bf
farwish <farwish@foxmail.com>
2017-09-28 19:42:17 +0800

    params

--- language_reference/Context_Options_and_Parameters/http_context_options.php
+++ language_reference/Context_Options_and_Parameters/http_context_options.php
@@ -36,7 +36,25 @@ $options = [
     'http' => [
         'method' => 'POST',
         'header' => 'Content-type: application/x-www-form-urlencoded',
-        'content'=> '',
+        'content'=> http_build_query([
+            'token' => '7633dd1b8e8d5fe20643e40d702230dfa7c8cf42',
+
+            'title' => '测试',
+            'start_time' => '2017-10-01 23:23',
+            'start_place' => '花海',
+            'type' => 1,
+            'placard' => '2.jpg',
+            'details' => '详情...',
+
+            //'activity_id' => 10,
+            //'apply_name' => 'aaa',
+            //'mobile' => '14398989897',
+
+            //'username' => 'lg',
+            //'password' => '123456',
+            //'time' => 1505998131,
+            //'sign' => '20e8db5d2e5f6f5497ffd3540b7aa13a4c1d773e',
+        ]),
 //        'max_redirects' => 0, // Ignore redirects.
 //        'ignore_errors' => 1, // Fetch the content even on failure status codes.
     ],
