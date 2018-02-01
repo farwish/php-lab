75197f2a9a43d21970b6131e3186339eef1f22f4
farwish <farwish@foxmail.com>
2018-02-02 02:26:17 +0800

    parse request header

--- lab/via/start.php
+++ lab/via/start.php
@@ -38,59 +38,93 @@ $con
     })
     // option, when client send message to server, callback trigger.if
     ->onMessage(function($client) {
+        $buffer             = '';
+        $method             = '';
+        $url                = '';
+        $protocol_version   = '';
+        $request_body       = '';
+        $request_header     = [];
+        $response_header    = [];
+        $end_of_header      = false;
 
-		$buffer = '';
+        // Can not stop !!!! body has no endup sign.
+        while ( ($buffer = fgets($client, 1024)) !== false ) {
+            if ($end_of_header) {
+                // Body.
+                $request_body .= $buffer;
+            } else {
+                if ($buffer == "\r\n") {
+                    // End of header.
+                    $end_of_header = true;
+                } else {
+                    // Header.
+                    $array = explode(' ', $buffer);
+                    if (count($array) > 2) {
+                        // Request line.
+                        $method           = $array[0];
+                        $url              = $array[1];
+                        $protocol_version = $array[2];
+                    } else {
+                        // Request header.
+                        list ($key, $value) = $array;
+                        $request_header[ rtrim($key, ':') ] = $value;
+                    }
+                }
+            }
+        }
 
-		// Prove the buffer is not empty.
-		// loop recvfrom bug.
-		//while (! preg_match('/\r?\n\r?\n/', $buffer)) {
-			//$buffer .= fread($client, 1024);
-		//}
+        // Prove the buffer is not empty.
+        // loop recvfrom bug.
+        //while (! preg_match('/\r?\n\r?\n/', $buffer)) {
+        //$buffer .= fread($client, 1024);
 
-		/* websocket request header
-			GET / HTTP/1.1
-			Host: 10.0.2.15:8080
-			Connection: Upgrade
-			Pragma: no-cache
-			Cache-Control: no-cache
-			Upgrade: websocket
-			Origin: http://127.0.0.1
-			Sec-WebSocket-Version: 13
-			User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/63.0.3239.84 Chrome/63.0.3239.84 Safari/537.36
-			Accept-Encoding: gzip, deflate
-			Accept-Language: en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7
-			Sec-WebSocket-Key: tefTZ4qXPIuVDHW08jzLJg==
-			Sec-WebSocket-Extensions: permessage-deflate; client_max_window_bits
-		 */
+        //}
 
-		/*
-		$buffer = '';
-		$sec_websocket_key = '';
-		while ( !preg_match('/\r?\n\r?\n/', $buffer) && ($buffer = fgets($client, 4096)) !== false ) {
-			if ( preg_match('/Sec-WebSocket-Key/', $buffer) ) {
-				$sec_websocket_key = substr($buffer, strpos($buffer, ' ') + 1);
-			}
-		}
+        /* websocket request header
+            GET / HTTP/1.1
+            Host: 10.0.2.15:8080
+            Connection: Upgrade
+            Pragma: no-cache
+            Cache-Control: no-cache
+            Upgrade: websocket
+            Origin: http://127.0.0.1
+            Sec-WebSocket-Version: 13
+            User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/63.0.3239.84 Chrome/63.0.3239.84 Safari/537.36
+            Accept-Encoding: gzip, deflate
+            Accept-Language: en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7
+            Sec-WebSocket-Key: tefTZ4qXPIuVDHW08jzLJg==
+            Sec-WebSocket-Extensions: permessage-deflate; client_max_window_bits
+         */
 
-		// server need response.
-		$header = "HTTP/1.1 101 Switching Protocols\r\n";
-		$header .= "Upgrade: websocket\r\n";
-		$header .= "Connection: Upgrade\r\n";
-		$header .= "Sec-WebSocket-Accept: " . base64_encode(sha1($sec_websocket_key . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11', true)) . "\r\n";
-		$header .= "Sec-WebSocket-Version: 13\r\n";
-		 */
+        /*
+        $buffer = '';
+        $sec_websocket_key = '';
+        while ( !preg_match('/\r?\n\r?\n/', $buffer) && ($buffer = fgets($client, 4096)) !== false ) {
+            if ( preg_match('/Sec-WebSocket-Key/', $buffer) ) {
+                $sec_websocket_key = substr($buffer, strpos($buffer, ' ') + 1);
+            }
+        }
 
+        // server need response.
+        $header = "HTTP/1.1 101 Switching Protocols\r\n";
+        $header .= "Upgrade: websocket\r\n";
+        $header .= "Connection: Upgrade\r\n";
+        $header .= "Sec-WebSocket-Accept: " . base64_encode(sha1($sec_websocket_key . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11', true)) . "\r\n";
+        $header .= "Sec-WebSocket-Version: 13\r\n";
+         */
 
-		$body = 'AAAA';
-		$header = "HTTP/1.1 200 OK\r\n";
-		$header .= "Connection: close\r\n";
-		$header .= "Content-Length: " . strlen($body) . "\r\n";
-		$header .= "Content-Type: text/html; charset=utf-8\r\n";
-		$header .= "Server: via\r\n";
-		$header .= "X-Powered-By: PHP/8\r\n";
-		$header .= "\r\n";
-		fwrite($client, $header . $body);
-		fclose($client);
+        /*
+        $body = 'AAAA';
+        $header = "HTTP/1.1 200 OK\r\n";
+        $header .= "Connection: close\r\n";
+        $header .= "Content-Length: " . strlen($body) . "\r\n";
+        $header .= "Content-Type: text/html; charset=utf-8\r\n";
+        $header .= "Server: via\r\n";
+        $header .= "X-Powered-By: PHP/8\r\n";
+        $header .= "\r\n";
+        fwrite($client, $header . $body);
+        fclose($client);
+         */
     })
 
     // Start server.
