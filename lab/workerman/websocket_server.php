982180686677f14e0a4e5a51b176895e525e9dfa
farwish <farwish@foxmail.com>
2018-04-18 19:27:01 +0800

    cron auto commit

--- lab/workerman/websocket_server.php
+++ lab/workerman/websocket_server.php
@@ -11,13 +11,15 @@ use Workerman\Worker;
 
 $ws_worker = new Worker("websocket://0.0.0.0:8081");
 
-$ws_worker->count = 3;
+$ws_worker->count = 2;
+$ws_worker->reusePort = true;
 
 $ws_worker->onConnect = function($connection) {
     echo "New connection\n";
 };
 
 $ws_worker->onMessage = function($connection, $data) {
+    print_r($connection);
     echo $data . "\n";
     while (1) {
         sleep(1);
