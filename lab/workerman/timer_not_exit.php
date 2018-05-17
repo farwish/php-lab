7179dba832f99bdd1a94afe6d5274f74cfd849fc
farwish <farwish@foxmail.com>
2018-05-17 18:13:22 +0800

    Timer will not exit when accur infinite loop

--- lab/workerman/timer_not_exit.php
+++ lab/workerman/timer_not_exit.php
@@ -12,19 +12,24 @@ use Workerman\Lib\Timer;
 
 // 不监听任何 socket 地址 ( 不创建套接字上下文 )
 // Worker.php:1568
-$worker = new Worker('text://127.0.0.1:8091');
+$worker = new Worker();
 
 $worker->count = 4;
 
-$worker->reusePort = true;
-
 $worker->onWorkerStart = function($worker) {
-    $time_interval = 2.5;
-
-    Timer::add($time_interval, function() {
-        echo "task run\n";
+    Timer::add(10, function() {
+        echo "this is task.\n";
         exit;
-    }, [], false);
+    });
+
+    try {
+        while(true) {
+
+        }
+        throw new \Exception('异常了');
+    } catch (\Exception $e) {
+        print_r('aaaaaaaaaaa');
+    }
 };
 
 Worker::runAll();
